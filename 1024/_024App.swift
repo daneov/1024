a46ccd556207c4317f698d29754bb679a8e706bf
Daneo van Overloop <7900887+daneov@users.noreply.github.com>
2024-11-07 08:54:49 +0100

    Initial commit

---
+++ 1024/_024App.swift
@@ -0,0 +1,17 @@
+//
+//  _024App.swift
+//  1024
+//
+//  Created by Daneo Van Overloop on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct _024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
